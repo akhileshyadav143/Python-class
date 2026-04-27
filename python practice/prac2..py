560edb19ed0f3e214547eb96be50649a34c95460
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-04-27 23:43:52 +0530

    python

---
+++ python practice/prac2..py
@@ -0,0 +1,37 @@
+# # marks=[85,94,79,96]
+# # print(marks[1:4])
+# # print(marks[1:])
+# # print(marks[-3:-2])
+
+# # list methods
+# # list.append   add elemnts at the end
+# # list.sort sprts imn asending order4
+# # list.revers reverse list
+# # list sort(reverse=true)   desending order
+# # list.insert(idx, el)  insert elemnt t index
+
+# list=[1,3,2]
+# list.append(6)
+# print(list)
+
+
+
+# list =[1,4,6,7,6]
+# print(list.sort())
+# print(list)
+
+
+
+# list=[2,4,6,7,4,7,]
+# print(list.sort(reverse=True))
+# print (list)
+
+
+# pop
+list=[6,9,8,6]
+list.pop(2)
+print(list)
+
+# tupllesuild dta us create immutable seqrnece of vlur
+#  ab
+
