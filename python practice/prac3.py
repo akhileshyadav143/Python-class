61eae37a549676d376da12af4437496f8f4838ec
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-07-18 23:31:51 +0530

    list

---
+++ python practice/prac3.py
@@ -0,0 +1,41 @@
+# marks=[94.5,45.3,6,7,446,67.7]
+# print(marks)
+# print(type(marks))
+# print(marks[0])
+
+
+# change    list-> mutable
+# student=["karan",95.7,"Delhi"]
+# print(student[0])
+# student[0]="Ram"
+# print(student)
+
+
+
+# slicing
+
+# marks=[86,89,76,89,45]
+# print(marks[1:3])
+# print(marks[-4:-1])
+
+
+
+#  mathods in list
+
+# list=[2,4,5]
+# list.append(4)
+# print(list)
+# list.sort()  #sorting
+# print(list)
+
+
+
+
+# li=['d','f','g','h']
+# li.reverse()
+# print(li)
+
+
+
+
+
---
+++ python practice/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+marks=[86,89,76,89,45]
+# print(marks[1:3])
+# print(marks[-4:-1])
