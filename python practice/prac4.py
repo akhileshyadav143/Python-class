de3c3c24c702f6929688d1cf6c105d1891d2fddb
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-07-19 23:22:37 +0530

    list1

---
+++ python practice/prac4.py
@@ -0,0 +1,22 @@
+# movies=[]
+# mov1= input("enter  1st movie:")
+# mov2= input("enter 2nd movie:")
+# mov3= input("enter 3rd movie:")
+
+
+# movies.append(mov1)
+# movies.append(mov2)
+# movies.append(mov3)
+
+
+
+list1=[1,2,1]
+list2=[1,2,3]
+
+copy_list1=list1.copy()
+copy_list1.reverse()
+
+if(copy_list1== list1):
+    print("palindeom")
+else:    
+    print("NOT palindrom")
