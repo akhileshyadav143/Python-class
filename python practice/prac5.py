b6bf9361b7e1189a23862ca4d64383c5e6bc3d2a
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-07-21 23:18:32 +0530

    Distionary1

--- python practice/prac5.py
+++ python practice/prac5.py
@@ -8,13 +8,20 @@
 # }
 # print(info)
 
-student={
-    "name": "happy kumar",
-    "subject":{
-      "phy":99,
-      "math": 98,
-      "che":90
-    }
-    }
-print(len( student))
-print(list (student.keys()))
+# student={
+#     "name": "happy kumar",
+#     "subject":{
+#       "phy":99,
+#       "math": 98,
+#       "che":90
+#     }
+#     }
+# print(len( student))
+# print(list (student.keys()))
+
+# new_dict={"name": "neha","age": 18}
+# student.update(new_dict)
+# print(student)
+
+
+
---
+++ python practice/prac6.py
@@ -0,0 +1,25 @@
+#set in python
+
+# collection={1,3,4,5,"world",6}
+
+# print(collection)
+# print(type(collection))
+# print(len(collection))
+
+
+# emptyset create
+# collection=set()
+# print(collection)
+
+
+
+
+# .........ADD.............
+
+collection=set()
+
+collection.add(1)
+collection.add(2)
+print(collection)
+
+print(collection.remove(1))
