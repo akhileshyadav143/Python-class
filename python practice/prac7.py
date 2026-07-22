13f06b35e8ff44f644c89b5f4439a46d69563c5f
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-07-22 23:09:25 +0530

    loop

---
+++ python practice/prac7.py
@@ -0,0 +1,59 @@
+# while
+
+# count=1
+# while  count<=5 :
+#     print("hello")
+#     count += 1
+#     print(count)
+
+#     i=1
+# while i<=5:
+#        print("hello")
+#        i += 1
+
+
+# print  number from 1to n
+# i=1
+# while i<=5 :
+#     print(i)
+#     i += 1
+
+# print("Loop ended")
+
+
+# print  1 to 100
+
+
+# i=1
+# while i<=100:
+#     print(i)
+#     i += 1
+
+# print("loop is ended")    
+
+
+
+# ''''''''''''''"'''  reverse''''''''''''''''
+
+# i=100
+# while i >= 1 :
+#        print(i)
+
+#        i -= 1
+# print('loop is ended')      
+
+
+# ''''''''''''''''''''''''' table print '''''''''''''
+# n=int(input("enter a number : " ))
+# i=1
+# while i<=10 :
+#     print(n*i)
+#     i += 1
+
+
+
+nums=[1,4,9,25,36,64,81,100]
+idx=0
+while idx < len(nums) :
+    print(nums[idx])
+    idx += 1
