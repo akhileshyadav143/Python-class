982b11941f44380dffab64632bf560a37fe85730
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-07-24 23:27:20 +0530

    loop

---
+++ python practice/prac9.py
@@ -0,0 +1,40 @@
+# i=1
+# while i<=10:
+#     if(i%2!=0):
+#         i +=1
+#         continue
+#     print(i)
+#     i +=1
+
+
+
+# nums=[1,2,3,4,5]
+
+# for val in nums:
+#     print(val)
+
+
+
+
+
+# tup =(1,2,3,8,6,7)
+
+# for num in tup:
+#     print(num)
+
+
+
+# str ="Akhilesh"
+
+# for char in str:
+#    print(char)
+
+
+
+
+# nums=[1,3,4,5,6,7,9,5,43]
+
+# for el in nums:
+#     print(el)
+
+
